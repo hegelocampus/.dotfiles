020dde59a9a9f9cbc3c8eed0335da589a72c6a97
hegelocampus <pizzaman8099@gmail.com>
2019-02-02 12:13:00 -0800

    replaced syntax-hightlight w fast, added send, added some aliases

--- zsh/custom/custom-aliases.zsh
+++ zsh/custom/custom-aliases.zsh
@@ -2,6 +2,9 @@ alias :q=exit
 alias :qw=:q
 alias :wq=:q
 alias cdot='cd ~/.dotfiles/'
+alias tread='t add project:school +Reading'
+alias gsub='git submodule add'
+alias gstat='git status'
 
 eval "$(hub alias -s)"
 
