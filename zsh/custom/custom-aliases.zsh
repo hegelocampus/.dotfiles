08d38208ea78a6c3a72aa8f5150660a7a19ac113
hegelocampus <pizzaman8099@gmail.com>
2019-02-08 17:18:02 -0800

    minor changes - added vim plugs and aliases

--- zsh/custom/custom-aliases.zsh
+++ zsh/custom/custom-aliases.zsh
@@ -2,9 +2,11 @@ alias :q=exit
 alias :qw=:q
 alias :wq=:q
 alias cdot='cd ~/.dotfiles/'
+alias cdoc='cd ~/Documents/'
 alias tread='t add project:school +Reading'
 alias gsub='git submodule add'
 alias gstat='git status'
+alias vim=nvim
 
 eval "$(hub alias -s)"
 
