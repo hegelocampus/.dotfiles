766fc4464f4783666cdfe871b8f12901850c35d5
hegelocampus <pizzaman8099@gmail.com>
2019-02-24 14:44:03 -0800

    lots of little changes, but nothing huge

--- zsh/custom/custom-aliases.zsh
+++ zsh/custom/custom-aliases.zsh
@@ -3,10 +3,13 @@ alias :qw=:q
 alias :wq=:q
 alias cdot='cd ~/.dotfiles/'
 alias cdoc='cd ~/Documents/'
+alias cdrive='cd ~/gdrive/'
 alias tread='t add project:school +Reading'
 alias gsub='git submodule add'
 alias gstat='git status'
 alias vim=nvim
+alias vi=nvim
+alias ou='ls -A'
 
 eval "$(hub alias -s)"
 
