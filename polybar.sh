a5cfed1e3ea1ed953f132bb4da745a5282aa92ff
Bee <pizzaman8099@gmail.com>
2019-07-31 20:37:42 -0700

    Minor changes, working on new polybar config
    Creating a new polybar config for use with an auxillary monitor

--- polybar.sh
+++ polybar.sh
@@ -9,7 +9,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar --config=/home/bee/.dotfiles/config/polybar mybar &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload --config=/home/bee/.dotfiles/config/polybar mybar &
+  done
+else
+  polybar --reload --config=/home/bee/.dotfiles/config/polybar mybar &
+fi
 
 # Set color scheme using wal
 cat /home/bee/.cache/wal/sequences
